8ad361d020b658e834a6f73d951abcc77fea5319
EstebanGC <johan.gal.car@gmail.com>
2024-02-05 22:18:58 -0500

    Adding Service and Repository

---
+++ fraud/src/main/java/com/microservices/fraud/FraudCheckHistoryService.java
@@ -0,0 +1,23 @@
+package com.microservices.fraud;
+
+import java.time.LocalDateTime;
+
+public class FraudCheckHistoryService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public FraudCheckHistoryService(FraudCheckHistoryRepository fraudCheckHistoryRepository){
+        this.fraudCheckHistoryRepository = fraudCheckHistoryRepository;
+    }
+
+    public boolean isFraudulentCustomer(Integer customerId){
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        return false;
+    }
+}
