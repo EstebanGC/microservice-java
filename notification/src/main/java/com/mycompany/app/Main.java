9a3ba3dd18fb36c3f3a9f810f5611e6c39c87955
EstebanGC <johan.gal.car@gmail.com>
2024-03-18 22:14:26 -0500

    ...

--- notification/src/main/java/com/mycompany/app/Main.java
+++ notification/src/main/java/com/mycompany/app/Main.java
@@ -2,6 +2,6 @@ package com.mycompany.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Prooving stuff with microservices");
     }
 }
