59f9589afc915cc6e532496192382cdc45be61f6
EstebanGC <johan.gal.car@gmail.com>
2024-03-25 21:37:57 -0500

    watever

--- notification/src/main/java/com/mycompany/app/Main.java
+++ notification/src/main/java/com/mycompany/app/Main.java
@@ -2,6 +2,7 @@ package com.mycompany.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prooving stuff with microservices");
+        System.out.println("Prooving stuff with microservices" +
+                "whatever!!!");
     }
 }
